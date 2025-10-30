4bd8a836a7f1d3885abd87ff9d96b12869b21b17
Victor M <victor.marques31415@gmail.com>
2025-10-30 18:25:25 -0300

    Atualizar o readcsv.py

--- readcsv.py
+++ readcsv.py
@@ -1,55 +1,27 @@
+from coleciona import Counter
 codify = input().strip()
 elec = []
-el1=0
-el2=0
-el3=0
-el4=0
-el5=0
-el6=0
-el7=0
-el8=0
+candidates = []
 try:
  with open("arquivo.csv", "r", encoding=codify) as f:
   data = f.readlines()
-  for linha in data:
+  for linha in data[1:]
    elec = linha.split(";")
-   if len(elec) < 2:
-    continue
-   if elec[1] == "C1":
-    el1 += 1
-   if elec[1] == "C2":
-    el2 += 1
-   if elec[1] == "C3":
-    el3 += 1 
-   if elec[1] == "C4":
-    el4 += 1
-   if elec[1] == "C5":
-    el5 += 1
-   if elec[1] == "C6":
-    el6 += 1
-   if elec[1] == "C7":
-    el7 += 1 
-   if elec[1] == "C8":
-    el8 += 1
+   if len(elec) >= 2:
+    candidate = elec[1].strip()
+    candidates = append(candidate)
 except FileNotFoundError:
  print("arquivo.csv não encontrado")
  exit()
 except UnicodeDecodeError:
  print(f"Erro ao codificar arquivo com {codify}")
  exit()
-votos = {
- "C1": el1,
- "C2": el2,
- "C3": el3,
- "C4": el4,
- "C5": el5,
- "C6": el6,
- "C7": el7,
- "C8": el8
-}
-most_voted = max(votos, key=votos.get)
-print(f"O candidato mais votado foi {most_voted} com {votos[most_voted]} votos.")
-
+votos = Counter(candidates)
+if votos:
+ most_voted = max(votos, key=votos.get)
+ print(f"O candidato mais votado foi {mais_votado} com {votos[mais_votado]} votos.")
+else:
+ print("Nenhum voto válido encontrado.")
 
   
   
