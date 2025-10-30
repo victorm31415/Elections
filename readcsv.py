4d8ca255fa11ca6cf443ce492139ac0eb895cd0f
Victor M <victor.marques31415@gmail.com>
2025-10-30 14:00:32 -0300

    Atualizar o readcsv.py

--- readcsv.py
+++ readcsv.py
@@ -1,4 +1,7 @@
 codify = input().strip()
 with open("arquivo.csv", "r", encoding=codify) as f:
  data = f.read()
- except FileNotFoundError
+ except FileNotFoundError:
+  print("arquivo.csv não encontrado")
+ except UnicodeDecodeError:
+  print("Erro ao codificar arquivo com {codify}")
