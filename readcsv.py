8e2bcb363d427e71328e003c853113cb3de5622e
Victor M <victor.marques31415@gmail.com>
2025-10-30 14:20:19 -0300

    Atualizar o readcsv.py

--- readcsv.py
+++ readcsv.py
@@ -1,8 +1,13 @@
 codify = input().strip()
+candidates = []
+elec = []
 with open("arquivo.csv", "r", encoding=codify) as f:
- data = f.read()
+ data = f.readlines()
  except FileNotFoundError:
   print("arquivo.csv não encontrado")
  except UnicodeDecodeError:
   print("Erro ao codificar arquivo com {codify}")
+ for linha in data:
+  elec = linha.split(";")
+  
  
