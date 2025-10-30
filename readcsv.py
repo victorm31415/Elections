8a0841a3e6d4f196b2bb088484a7695e178aa579
Victor M <victor.marques31415@gmail.com>
2025-10-30 18:05:57 -0300

    Atualizar o readcsv.py

--- readcsv.py
+++ readcsv.py
@@ -8,28 +8,29 @@ el5=0
 el6=0
 el7=0
 el8=0
-with open("arquivo.csv", "r", encoding=codify) as f:
- data = f.readlines()
- for linha in data:
-  elec = linha.split(";")
-  if len(ele) < 2:
-   continue
-  if elec[1] == "C1":
-   el1 += 1
-  if elec[1] == "C2":
-   el2 += 1
-  if elec[1] == "C3":
-   el3 += 1 
-  if elec[1] == "C4":
-   el4 += 1
-  if elec[1] == "C5":
-   el5 += 1
-  if elec[1] == "C6":
-   el6 += 1
-  if elec[1] == "C7":
-   el7 += 1 
-  if elec[1] == "C8":
-   el8 += 1
+try:
+ with open("arquivo.csv", "r", encoding=codify) as f:
+  data = f.readlines()
+  for linha in data:
+   elec = linha.split(";")
+   if len(elec) < 2:
+    continue
+   if elec[1] == "C1":
+    el1 += 1
+   if elec[1] == "C2":
+    el2 += 1
+   if elec[1] == "C3":
+    el3 += 1 
+   if elec[1] == "C4":
+    el4 += 1
+   if elec[1] == "C5":
+    el5 += 1
+   if elec[1] == "C6":
+    el6 += 1
+   if elec[1] == "C7":
+    el7 += 1 
+   if elec[1] == "C8":
+    el8 += 1
 except FileNotFoundError:
  print("arquivo.csv não encontrado")
  exit()
