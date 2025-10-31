8a6d6d38f6b64e1f785c9713e4288946607cea29
Victor M <victor.marques31415@gmail.com>
2025-10-30 21:57:53 -0300

    Atualizar o percentvotes.py

--- percentvotes.py
+++ percentvotes.py
@@ -4,9 +4,11 @@ codify = input("Digite a codificação:")
 votes_per_city = defaultdict(list)
 try:
  with open("arquivo.csv", "r", encoding=codify) as f:
-  dados = csv.reader(f, delimitar=";")
+  dados = csv.reader(f, delimiter=";")
   next(dados, None)
   for elec in dados:
    if len(elec) >=2:
-    city = elec[0].strip.title()
-    candidate = elec[1].strip.upper()
+    city = elec[0].strip().title()
+    candidate = elec[1].strip().upper()
+    votes_per_city[city].append(candidate)
+
