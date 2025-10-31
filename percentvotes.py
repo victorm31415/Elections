d52a524aac09e38235fbad87db8589112ddbf082
Victor M <victor.marques31415@gmail.com>
2025-10-30 22:14:41 -0300

    Atualizar o percentvotes.py

--- percentvotes.py
+++ percentvotes.py
@@ -3,15 +3,20 @@ from collections import defaultdict, Counter
 codify = input("Digite a codificação:")
 votes_per_city = defaultdict(list)
 try:
- with open("arquivo.csv", "r", encoding=codify) as f:
-  dados = csv.reader(f, delimiter=";")
-  next(dados, None)
-  for elec in dados:
-   if len(elec) >=2:
-    city = elec[0].strip().title()
-    candidate = elec[1].strip().upper()
-    votes_per_city[city].append(candidate)
+  with open("arquivo.csv", "r", encoding=codify) as f:
+   dados = csv.reader(f, delimiter=";")
+   next(dados, None)
+   for elec in dados:
+    if len(elec) >=2:
+     city = elec[0].strip().title()
+     candidate = elec[1].strip().upper()
+     votes_per_city[city].append(candidate)
 except FileNotFoundError:
- print(f"Não foi possível encontrar o arquivo")
+  print(f"Não foi possível encontrar o arquivo")
 except UnicodeDecodeError:
- print(f"Erro ao decodificar arquivo com {codify}")
+  print(f"Erro ao decodificar arquivo com {codify}")
+for city, candidates in votes_per_city.items():
+  total_votes = len(candidates)
+  counter = Counter(candidates)
+
+
